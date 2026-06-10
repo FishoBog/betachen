316d1755ea3f197ce3b5006968b0a78949e46279
FishoBog <fishobog@gmail.com>
2026-06-09 20:23:23 -0700

    Update route.ts

--- src/app/api/admin/verify/route.ts
+++ src/app/api/admin/verify/route.ts
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { createClient } from "@supabase/supabase-js";
 import { NextRequest, NextResponse } from "next/server";
 
-const ADMIN_USER_ID = "user_3AmnQEFKPsp6EX1W9xl88nOW4AV";
+const ADMIN_USER_ID = "user_3BeYdNiwHjIpWA8iw63QXV5Yb6Y";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
